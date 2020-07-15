52f66ef04b30581853fc52f590ebdee2f5dd8c99
mahmoud <mahmoud.kheyrati.fard@gmail.com>
2020-07-15 08:59:54 +0430

    finish Algorithm and tests

--- main.c
+++ main.c
@@ -1,32 +1,7 @@
 #include "cli/cli.h"
-#include "diffChecker/stringDiffSolver.h"
 #include "examples/diffCheckerExamples/createBaseOnChangesExamples.h"
 int main(int argc, String *argv) {
-//    createSingleFileWithChangesExample();
+    createSingleFileWithChangesExample();
 //    runCli(argc,argv);
-    String A = "abcd";
-    String B = "ab";
-
-
-
-
-    A= readFile("./test","old.txt");
-    B=readFile("./test","new.txt");
-
-    long int lenA = strlen(A);
-    long int lenB = strlen(B);
-
-
-    int** lookupTable = createLookupTable(A,B);
-    struct DifferenceList* differenceList = parsLookUpTable(lookupTable,A,B,lenA,lenB);
-
-    struct DifferenceList *list = differenceList;
-    struct DifferenceList *list2 = StringDiffChecker(A, B);
-
-    diffSaver(list, "./test", "myChanges.txt");
-    struct DifferenceList* changeList = diffReader("./test","myChanges.txt");
-    nextSequenceGenerator(A,changeList,"./created","basedOnChanges.txt");
-
-
 
 }
